f575cd5724a8a26ac00b887ee17431eb20d0580a
Daria <kotikgriga0027@gmail.com>
2023-02-05 09:52:20 +0300

    Solving the third problem 2

--- ex_3/Program.cs
+++ ex_3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 2, 89, 312, 48, 15, 961, 15 };
+int n = array.Length;
+
+int find = 15;
+
+int index = 0;
+while (index < n) {
+    if (array[index] == find) {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
